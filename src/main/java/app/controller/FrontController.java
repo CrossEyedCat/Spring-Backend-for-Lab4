f2541f13ec888bf7d8484cfeadb56a95d814421c
DeltaThreeEight <sawin.georgij@yandex.ru>
2020-01-16 01:19:18 +0300

    set server port explicitly, frontend controller improvement

--- src/main/java/app/controller/FrontController.java
+++ src/main/java/app/controller/FrontController.java
@@ -1,12 +1,13 @@
 package app.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Map;
 
 @Controller
-public class FrontController {
+public class FrontController implements ErrorController {
     @GetMapping("/auth/*")
     public String auth(Map<String, Object> model) {
         return "/index.html";
@@ -15,5 +16,15 @@ public class FrontController {
     @GetMapping("/main")
     public String main(Map<String, Object> model) {
         return "/index.html";
+    }
+
+    @GetMapping("/error")
+    public String errors(Map<String, Object> model) {
+        return "/index.html";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
     }
 }
